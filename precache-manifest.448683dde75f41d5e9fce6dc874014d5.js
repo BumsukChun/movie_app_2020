be3b000b628fb51db00530af653c4a580d198852
Bumsuk Chun <bumsukchun@gmail.com>
2020-09-09 05:54:13 +0900

    Updates

---
+++ precache-manifest.448683dde75f41d5e9fce6dc874014d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d7fce27b61a0ac20d655abf6988695",
+    "url": "/BumsukChun/movie_app_2020/index.html"
+  },
+  {
+    "revision": "b1bb7b6d339f3ec52933",
+    "url": "/BumsukChun/movie_app_2020/static/css/main.4fa2f709.chunk.css"
+  },
+  {
+    "revision": "dcbc1ad3746847d335f9",
+    "url": "/BumsukChun/movie_app_2020/static/js/2.968ebb96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BumsukChun/movie_app_2020/static/js/2.968ebb96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1bb7b6d339f3ec52933",
+    "url": "/BumsukChun/movie_app_2020/static/js/main.821a6907.chunk.js"
+  },
+  {
+    "revision": "7097873bf46016c73a82",
+    "url": "/BumsukChun/movie_app_2020/static/js/runtime-main.f5a43835.js"
+  }
+]);
