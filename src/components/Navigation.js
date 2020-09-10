b896dd3bac768dadd844ef57c3272123b12dd493
Bumsuk Chun <bumsukchun@gmail.com>
2020-09-10 17:06:12 +0900

    ajax test

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import './Navigation.css';
+import axios from 'axios';
+// axios is like fetch()
 
 function Navigation() {
+
+    function test() {
+        axios.get('/#/about')
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+}
+
     return (
         <div className="nav">
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
+            <button type="button" onClick={test}>Change Content</button>
         </div>
     );
   }
